0bc80934ddd9d0ee253f8eddcded145c229e0667
unknown <1210316415@gitam.in>
2020-07-13 16:31:46 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            float average;
+            Console.WriteLine("enter three numbers");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+            average = (n1 + n2 + n3) / 3;
+            Console.WriteLine("average of three numbers is" + average);
+
+
+        }
+    }
+}
